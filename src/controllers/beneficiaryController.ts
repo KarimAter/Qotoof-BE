2450f73c298da58d0d7a4079c5b1bfdb33c47e8a
Karim Ater <karim.abdelbaky1@vodafone.com>
2022-05-17 21:20:45 +0200

    adjusting edit,delete checks

--- src/controllers/beneficiaryController.ts
+++ src/controllers/beneficiaryController.ts
@@ -1,14 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 import Beneficiary from '../models/Beneficiary'
 const getBeneficiaries = (req:Request, res:Response, next:NextFunction) => {
-  res.status(200).json(Beneficiary.fetchCases());
+  res.json(Beneficiary.fetchCases());
 }
 const postBeneficiary = (req:Request, res:Response, next:NextFunction) => {
     const beneficiary = new Beneficiary(req.body.beneficiaryName);
     beneficiary.save();
-    res
-      .status(201)
-      .json({ message: `Beneficiary is added successfully`, beneficiaryName: beneficiary.name });
+    res.json({
+      message: `Beneficiary is added successfully`,
+      beneficiaryName: beneficiary.name,
+    });
   }
   
 const editBeneficiary = (req:Request, res:Response, next:NextFunction) => {
@@ -16,8 +17,8 @@ const editBeneficiary = (req:Request, res:Response, next:NextFunction) => {
  const currentName = req.params.beneficiaryName;
  const ben = req.body as Beneficiary;
  const targetName = ben.name;
- Beneficiary.editBeneficiary(currentName,targetName);
- res.status(201).json({ message: `Name edited to ${targetName}` });
+ const message: string = Beneficiary.editBeneficiary(currentName, targetName);
+ res.json({ message: `${message} ${targetName}` });
 
 }
 
@@ -25,8 +26,8 @@ const editBeneficiary = (req:Request, res:Response, next:NextFunction) => {
 const deleteBeneficiary = (req:Request, res:Response, next:NextFunction) => {
 
   const currentName = req.params.beneficiaryName;
-  Beneficiary.deleteBeneficiary(currentName);
-  res.status(201).json({ message: `${currentName} deleted!` });
+ const message:string=Beneficiary.deleteBeneficiary(currentName);
+  res.json({ message: `${currentName} ${message}!` });
  }
 
 
--- src/models/Beneficiary.ts
+++ src/models/Beneficiary.ts
@@ -15,18 +15,20 @@ class Beneficiary implements IBeneficiary {
     beneficiaryList.push(this);
   }
 
-  static editBeneficiary(currentName: string, targetName: string) {
+  static editBeneficiary(currentName: string, targetName: string):string {
     const index: number = beneficiaryList.findIndex(
       (ben) => ben.name === currentName
     );
-    beneficiaryList[index] = new Beneficiary(targetName);
+    if (index !== -1) {beneficiaryList[index] = new Beneficiary(targetName);return 'Name edited to '};
+    return "Not found";
   }
-  static deleteBeneficiary(name: string) {
+  static deleteBeneficiary(name: string):string {
     const index: number = beneficiaryList.findIndex(
       (ben) => ben.name === name
     );
 
-    beneficiaryList.splice(index,1)
+    if (index !== -1) {beneficiaryList.splice(index, 1);return 'Deleted!'};
+     return "Not found";
   }
 
   static fetchCases(): IBeneficiary[] {
